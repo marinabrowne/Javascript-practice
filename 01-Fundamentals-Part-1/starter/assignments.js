78b2e979e3489ac9b94b260e92b8e97c003591e6
marinabrowne <marina_browne@hotmail.co.uk>
2024-02-25 14:43:56 +0000

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,37 @@
+const country = "United Kingdom";
+const continent = "Europe";
+
+
+console.log(country);
+console.log(continent);
+
+
+
+const isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English";
+
+let population = "67000000";
+console.log(population / 2);
+
+console.log(population + 1);
+
+let ukPopulation = 67000000;
+let finlandPopulation = 6000000;
+console.log(ukPopulation > finlandPopulation);
+
+let averagePopulation = 33000000;
+console.log(ukPopulation < averagePopulation);
+
+const description1 = "Portugal is in Europe, and its 11 million people speak portuguese";
+
+
+
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,65 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Marina");
+
+let firstName = "Marina";
+console.log(firstName);
+
+Variable name convention
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+console.log(x);
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
